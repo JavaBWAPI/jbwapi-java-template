02c1dbb57e65331e4409b412ed4129040c933701
JasperGeurtz <jcgeurtz@gmail.com>
2020-03-04 20:38:09 +0100

    setup small hello world bot

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# jbwapi-bot-template
-JBWAPI-Bot-Template
+#  JBWAPI Bot Template
+
+A small template hello world bot for JBWAPI users to ease setup.
+
+### Usage
+```
+$ git clone https://github.com/JavaBWAPI/jbwapi-bot-template.git
+
+$ cd jbwapi-bot-template
+
+$ mvn package
+
+$ java -jar target/jbwapi-template-1.0-SNAPSHOT-jar-with-dependencies.jar
+```
---
+++ src/main/java/Bot.java
@@ -0,0 +1,26 @@
+import bwapi.BWClient;
+import bwapi.DefaultBWListener;
+import bwapi.Game;
+
+import java.util.stream.Collectors;
+
+public class Bot extends DefaultBWListener {
+    private BWClient bwClient;
+    private Game game;
+
+    @Override
+    public void onStart() {
+        game = bwClient.getGame();
+    }
+
+    @Override
+    public void onFrame() {
+        game.drawTextScreen(100, 100, "Hello World!");
+    }
+
+    public static void main(String[] args) {
+        Bot bot = new Bot();
+        bot.bwClient = new BWClient(bot);
+        bot.bwClient.startGame();
+    }
+}
